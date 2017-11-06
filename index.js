9024bcb4f73a06895cb09b7288e2d1d4e8e0c3fa
Obi <obinnawalker@gmail.com>
2017-11-06 15:54:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,9 +85,10 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber === null || undefined){
-    return "Sorry, we don\'t have a credit card on file for you."
-  }
+    console.log("Sorry, we don\'t have a credit card on file for you.")
+  }else{
   console.log("Your total cost is $" +total()+ ", which will be charged to the card" +cardNumber)
 
   cart = []
+  }
 }
